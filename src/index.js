a32d839a4cb30f001cc056b12c9f5a5e10965ca4
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-08-05 19:53:07 -0500

    context and mongo*

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ app.get('/', (req, res) => {
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
     schema: schema,
-
+    context: {
+        messageId: 'test'
+    }
 }));
 
 app.listen(3000, () => console.log('server running on port 3000'))
